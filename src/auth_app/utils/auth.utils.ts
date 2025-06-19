aa94b4e3bd3cb7eb26ec731060b44262e45ba37e
akashpg-M <waybooster.pg@gmail.com>
2025-06-19 19:25:04 +0530

    project finished

--- src/auth_app/utils/auth.utils.ts
+++ src/auth_app/utils/auth.utils.ts
@@ -9,7 +9,8 @@ export const generateToken = (userId: string, res: Response): void => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     httpOnly: true, // Prevent XSS attacks
-    sameSite: "strict", // Prevent CSRF attacks
+    sameSite: process.env.NODE_ENV === "production" ? ("none" as const) : ("lax" as const),
     secure: process.env.NODE_ENV !== "development",
+    path: "/", // To Ensure cookie is accessible on all routes
   });
 };

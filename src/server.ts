2f9c1c1301124457aa5b9e8dff3ca5b961dfd70c
akashpg-M <waybooster.pg@gmail.com>
2025-06-20 08:58:28 +0530

    project finished

--- src/server.ts
+++ src/server.ts
@@ -19,8 +19,13 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH','DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
+    exposedHeaders: ['Set-Cookie'],
   })
 );
+
+app.options('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
